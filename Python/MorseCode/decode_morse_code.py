0f9cd3ed3d9ed3bff2d91c4e42cf173dd97d993b
Aravind A <118422089+aarav-26@users.noreply.github.com>
2025-05-08 11:03:48 +0530

    Add files via upload

---
+++ Python/MorseCode/decode_morse_code.py
@@ -0,0 +1,39 @@
+symbols = {
+    ".-": "a",
+    "-...": "b",
+    "-.-.": "c",
+    "-..": "d",
+    ".": "e",
+    "..-.": "f",
+    "--.": "g",
+    "....": "h",
+    "..": "i",
+    ".---": "j",
+    "-.-": "k",
+    ".-..": "l",
+    "--": "m",
+    "-.": "n",
+    "---": "o",
+    ".--.": "p",
+    "--.-": "q",
+    ".-.": "r",
+    "...": "s",
+    "-": "t",
+    "..-": "u",
+    "...-": "v",
+    ".--": "w",
+    "-..-": "x",
+    "-.--": "y",
+    "--..": "z",
+    "/": " "  # Handling spaces between words
+}
+
+morse_code = input("Enter Morse Code: ").strip()
+morse_chars = morse_code.split(" ")
+decoded_message = ""
+
+for char in morse_chars:
+    if char in symbols:
+        decoded_message += symbols[char]
+
+print("Decoded message: ", decoded_message)
